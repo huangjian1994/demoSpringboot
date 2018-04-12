a1f5fb5f446ce055ac90d6e4e955c0535183d429
HuangJian <173247429@qq.com>
2018-04-12 15:31:20 +0800

    service

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.User;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author by hj on 2018-2-8.
